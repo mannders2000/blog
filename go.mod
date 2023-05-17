c9d80ae9ffb64775fe82bef7837ab2b4a5709252
Matt Anderson <>
2023-05-17 11:03:44 -0500

    Added files

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/matta9001/primehealthai
+
+go 1.19
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.16
+	github.com/ory/client-go v1.1.27
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", indexHandler).Methods("GET")
+	router.HandleFunc("/blog/{blogID}", blogHandler).Methods("GET")
+	router.PathPrefix("/public/").HandlerFunc(getPublic)
+
+	fmt.Println("starting on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("public/templates/header.tmpl", "public/templates/footer.tmpl", "public/html/index.html"))
+	err := tmpl.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func blogHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	blogID := vars["blogID"]
+
+	tmpl := template.Must(template.ParseFiles("public/templates/header.tmpl", "public/templates/footer.tmpl", fmt.Sprintf("public/html/blog/%s", blogID)))
+	err := tmpl.ExecuteTemplate(w, fmt.Sprintf("%s.html", blogID), nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getPublic(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	filePath := filepath.Join(".", path)
+
+	if strings.HasPrefix(path, "/public/css") {
+		w.Header().Set("Content-Type", "text/css")
+	} else if strings.HasPrefix(path, "/public/js") {
+		w.Header().Set("Content-Type", "application/javascript")
+	} else if strings.HasPrefix(path, "/public/images") {
+		w.Header().Set("Content-Type", "image/jpeg")
+	} else if strings.HasPrefix(path, "/data/images") {
+		w.Header().Set("Content-Type", "image/png")
+	} else if strings.HasPrefix(path, "/public/fonts") {
+		w.Header().Set("Content-Type", "fonts/font")
+	}
+
+	http.ServeFile(w, r, filePath)
+}
