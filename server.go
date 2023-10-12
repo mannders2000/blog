b62489a8e00b80285469d7a5c40e8409f791db4f
Matt Anderson <matta9001@gmail.com>
2023-10-11 21:33:10 -0500

    Big changes!

--- server.go
+++ server.go
@@ -19,6 +19,8 @@ func main() {
 	router.HandleFunc("/blog", getBlogHandler).Methods("GET")
 	router.HandleFunc("/blog/{blogID}", blogHandler).Methods("GET")
 
+	router.HandleFunc("/project/{projectID}", projectHandler).Methods("GET")
+
 	router.HandleFunc("/book", bookHandler).Methods("GET")
 
 	router.PathPrefix("/public/").HandlerFunc(getPublic)
@@ -62,6 +64,17 @@ func blogHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func projectHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	projectID := vars["projectID"]
+
+	tmpl := template.Must(template.ParseFiles("public/templates/header.tmpl", "public/templates/footer.tmpl", fmt.Sprintf("public/html/projects/%s.html", projectID)))
+	err := tmpl.ExecuteTemplate(w, fmt.Sprintf("%s.html", projectID), nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func getPublic(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	filePath := filepath.Join(".", path)
@@ -72,7 +85,7 @@ func getPublic(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/javascript")
 	} else if strings.HasPrefix(path, "/public/images") {
 		w.Header().Set("Content-Type", "image/jpeg")
-	} else if strings.HasPrefix(path, "/data/images") {
+	} else if strings.HasPrefix(path, "/public/images") {
 		w.Header().Set("Content-Type", "image/png")
 	} else if strings.HasPrefix(path, "/public/fonts") {
 		w.Header().Set("Content-Type", "fonts/font")
